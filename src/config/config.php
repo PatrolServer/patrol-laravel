9162e8d7bdf3f64853517c5087493b1b7adcef74
Matthias <me@matvp.info>
2016-02-25 13:42:53 +0100

    Config info

--- src/config/config.php
+++ src/config/config.php
@@ -2,10 +2,32 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | PatrolServer API Credentials
+    |--------------------------------------------------------------------------
+    |
+    | In order to work with the API, your key and secret need to be filled in
+    | below. You can find your unique key / secret pair in the PatrolServer
+    | dashboard, under the API menu.
+    |
+    */
+
     'key' => '',
 
     'secret' => '',
 
-    'enable_webhooks' => true
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Default Webhooks
+    |--------------------------------------------------------------------------
+    |
+    | A basic auto-update for your Laravel packages is included when you
+    | enable the default webhooks behavior. More information about auto
+    | updating your dependencies can be found on the GitHub page.
+    |
+    */
+
+    'enable_webhooks' => false
 
 ];
